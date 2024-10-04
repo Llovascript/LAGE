110e31429e72597661630e5a92e64484216b5919
Llovascript <157436170+Llovascript@users.noreply.github.com>
2024-10-04 15:25:00 -0600

    plantilla

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+# Página de inicio (login)
+@app.route("/")
+def index():
+    return render_template('dash.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
