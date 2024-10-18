d7364800f9305f5396c4e1464dc4d087dd11b27f
Llovascript <157436170+Llovascript@users.noreply.github.com>
2024-10-17 18:15:59 -0600

    Descarga de pdf y card de la directora 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, send_from_directory
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('dash.html')
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
